e0e256fccb2004844c0577b01f88294cdc0f9f50
x3rt <51767230+x3rt@users.noreply.github.com>
2023-03-29 17:02:34 -0600

    fix Package name

--- doc.go
+++ doc.go
@@ -1,24 +1,25 @@
-// Package statping is a server monitoring application that includes a status page server. Visit the Statping repo at
+// Package statping_ng is a server monitoring application that includes a status page server. Visit the Statping repo at
 // https://github.com/adamboutcher/statping-ng to get a full understanding of what this application can do.
 //
-// Install Statping
+// # Install Statping
 //
 // Statping is available for Mac, Linux and Windows 64x. You can download the tar.gz file or use a couple other methods. Download
 // the latest release at https://github.com/statping-ng/statping-ng/releases/latest or view below. If you're on windows, download
 // the zip file from the latest releases link.
 //
-//		// MacOS using homebrew
-//		brew tap statping/statping
-//		brew install statping
+//	// MacOS using homebrew
+//	brew tap statping/statping
+//	brew install statping
 //
-//		// Linux installation
-//		curl -o- -L https://statping.com/install.sh | bash
-//		statping version
+//	// Linux installation
+//	curl -o- -L https://statping.com/install.sh | bash
+//	statping version
 //
-// Docker
+// # Docker
 //
 // Statping can be built in many way, the best way is to use Docker!
-//		docker run -it -p 8080:8080 statping/statping
+//
+//	docker run -it -p 8080:8080 statping/statping
 //
 // Enjoy Statping and tell me any issues you might be having on Github. https://github.com/adamboutcher
 package statping_ng
